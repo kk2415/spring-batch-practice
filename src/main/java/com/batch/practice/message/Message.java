840462a2f1ba1c06e38c0a2080b040c351952a5f
Kim kyunghee <kkh2415@naver.com>
2022-07-20 18:17:29 +0900

    add : #18-Message 엔티티에 생성 메서드 추가

--- src/main/java/com/batch/practice/message/Message.java
+++ src/main/java/com/batch/practice/message/Message.java
@@ -7,6 +7,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -21,5 +23,31 @@ public class Message extends IdEntity {
 
     @Column(nullable = false, columnDefinition = "text")
     String content;
+
+    public Message(String userId, String title, String content) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public static Message expiredPointMessageInstance(String userId, LocalDate expiredDate, Long expiredAmount) {
+        return new Message(
+            userId,
+            String.format("%s 포인트 만료", expiredAmount.toString()),
+            String.format(
+                "%s 기준 %s 포인트가 만료되었습니다.", expiredDate.format(DateTimeFormatter.ISO_DATE) ,expiredAmount
+            )
+        );
+    }
+
+    public static Message expireSoonPointMessageInstance(String userId, LocalDate expiredDate, Long expiredAmount) {
+        return new Message(
+                userId,
+                String.format("%s 포인트 예정", expiredAmount.toString()),
+                String.format(
+                        "%s 까지 %s 포인트가 만료 예정입니다.", expiredDate.format(DateTimeFormatter.ISO_DATE) ,expiredAmount
+                )
+        );
+    }
 
 }
